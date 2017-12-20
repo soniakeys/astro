6377ae5c029356562b4fea5535a8cb54c672fc1a
Sonia <soniakeys@gmail.com>
2017-12-20 18:24:24 -0500

    add FFCalendarGregorianToJD, rearrange other date functions a bit

--- julian.go
+++ julian.go
@@ -4,12 +4,16 @@ package astro
 
 import "time"
 
-// CalendarGregorianToJD converts a Gregorian year, month, and day of month
-// to Julian day.
+// MeeusCalendarGregorianToJD converts a Gregorian year, month, and day of
+// month to Julian day.
 //
 // Negative years are valid, back to JD 0.  The result is not valid for
 // dates before JD 0.
-func CalendarGregorianToJD(y, m int, d float64) float64 {
+//
+// The algorithm by Meeus relies on floor truncation like the Fortran INT
+// function.  The implemenation here retains the floor truncation, which
+// makes this algorithm relatively slow.
+func MeeusCalendarGregorianToJD(y, m int, d float64) float64 {
 	switch m {
 	case 1, 2:
 		y--
@@ -22,6 +26,53 @@ func CalendarGregorianToJD(y, m int, d float64) float64 {
 		float64(floorDiv(306*(m+1), 10)+b) + d - 1524.5
 }
 
+// FFCalendarGregorianToJD converts a Gregorian year, month, and day of
+// month to Julian day.
+//
+// Implementation is by the widely used formula of Fliegel and van Flandern.
+// It is fast, although the formula appears rather cryptic.
+func FFCalendarGregorianToJD(y, m int, d float64) float64 {
+	return d - .5 + float64(-32075+1461*(y+4800+(m-14)/12)/4+
+		367*(m-2-(m-14)/12*12)/12-3*((y+4900+(m-14)/12)/100)/4)
+}
+
+// CalendarGregorianToMJD converts a Gregorian year, month, and day of
+// month to Modified Julian Day (MJD).
+//
+// The algorithm here is different than either the Meeus or Fliegel -
+// van Flandern Julian date algorithms.  It is more intuitive than either
+// yet runs about as fast as the Fliegel - van Flandern algorithm.
+//
+// It is not valid for proleptic dates with negative years.
+func CalendarGregorianToMJD(y, m int, d float64) float64 {
+	if m <= 2 {
+		y--
+	}
+	return float64(int(daysSinceFeb[m])+365*y+y/4-y/100+y/400-678882) + d
+}
+
+type dsf int
+
+var daysSinceFeb = [...]dsf{
+	0, jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec}
+
+const (
+	// at the start of mar 1, there have been 0 days
+	mar dsf = 0
+	// for apr, add the 31 days of mar
+	apr = mar + 31
+	may = apr + 30
+	jun = may + 31
+	jul = jun + 30
+	aug = jul + 31
+	sep = aug + 31
+	oct = sep + 30
+	nov = oct + 31
+	dec = nov + 30
+	jan = dec + 31
+	feb = jan + 31
+)
+
 // TimeToJD takes a Go time.Time and returns a JD as float64.
 //
 // Any time zone offset in the time.Time is ignored and the time is
@@ -31,5 +82,5 @@ func TimeToJD(t time.Time) float64 {
 	y, m, _ := ut.Date()
 	d := ut.Sub(time.Date(y, m, 0, 0, 0, 0, 0, time.UTC))
 	// time.Time is always Gregorian
-	return CalendarGregorianToJD(y, int(m), float64(d)/float64(24*time.Hour))
+	return FFCalendarGregorianToJD(y, int(m), float64(d)/float64(24*time.Hour))
 }
---
+++ julian_test.go
@@ -0,0 +1,119 @@
+// Public domain.
+
+package astro_test
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/soniakeys/astro"
+	"github.com/soniakeys/meeus/julian"
+)
+
+func ExampleMeeusCalendarGregorianToJD() {
+	// Meeus example 7.a, p. 61.
+	jd := astro.MeeusCalendarGregorianToJD(1957, 10, 4.81)
+	fmt.Printf("%.2f\n", jd)
+	// Meeus example 7.c, p. 64.
+	jd1 := astro.MeeusCalendarGregorianToJD(1910, 4, 20)
+	jd2 := astro.MeeusCalendarGregorianToJD(1986, 2, 9)
+	fmt.Println(jd2 - jd1)
+	// Output:
+	// 2436116.31
+	// 27689
+}
+
+func ExampleFFCalendarGregorianToJD() {
+	// Meeus example 7.a, p. 61.
+	jd := astro.FFCalendarGregorianToJD(1957, 10, 4.81)
+	fmt.Printf("%.2f\n", jd)
+	// Meeus example 7.c, p. 64.
+	jd1 := astro.FFCalendarGregorianToJD(1910, 4, 20)
+	jd2 := astro.FFCalendarGregorianToJD(1986, 2, 9)
+	fmt.Println(jd2 - jd1)
+	// Output:
+	// 2436116.31
+	// 27689
+}
+
+func ExampleTimeToJd() {
+	// Meeus example 7.a, p. 61.
+	t := time.Date(1957, 10, 4, 0, 0, 0, 0, time.UTC)
+	ns := 0.81 * float64(24*time.Hour)
+	t = t.Add(time.Duration(ns))
+	fmt.Printf("%.2f\n", astro.TimeToJD(t))
+	// Output:
+	// 2436116.31
+}
+
+func TestFF(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	j0 := julian.CalendarGregorianToJD(1582, 11, 1)
+	j1 := julian.CalendarGregorianToJD(10000, 1, 1)
+	dj := j1 - j0
+	for i := 0; i < 1e5; i++ {
+		j := j0 + dj*rand.Float64()
+		y, m, d := julian.JDToCalendar(j)
+		{
+			ja := astro.MeeusCalendarGregorianToJD(y, m, d)
+			if dj := math.Abs(ja - j); dj > 1e-6 {
+				t.Logf("%d-%02d-%f", y, m, d)
+				t.Logf("ja: want %f got %f diff %f", j, ja, dj)
+			}
+		}
+		{
+			jf := astro.FFCalendarGregorianToJD(y, m, d)
+			if dj := math.Abs(jf - j); dj > 1e-6 {
+				t.Logf("%d-%02d-%f", y, m, d)
+				t.Logf("jf: want %f got %f diff %f", j, jf, dj)
+			}
+		}
+		{
+			jm := astro.CalendarGregorianToMJD(y, m, d) + 240000.5
+			if dj := math.Abs(jm - j); dj > 1e-6 {
+				t.Logf("%d-%02d-%f", y, m, d)
+				t.Logf("jm: want %f got %f diff %f", j, jm, dj)
+			}
+		}
+	}
+}
+
+func BenchmarkFF(b *testing.B) {
+	rand.Seed(time.Now().UnixNano())
+	j0 := julian.CalendarGregorianToJD(1582, 11, 1)
+	j1 := julian.CalendarGregorianToJD(2582, 11, 1)
+	dj := j1 - j0
+	type ymd struct {
+		y, m int
+		d    float64
+	}
+	cs := make([]ymd, 1e6)
+	for i := range cs {
+		y, m, d := julian.JDToCalendar(j0 + dj*rand.Float64())
+		cs[i] = ymd{y, m, d}
+	}
+	b.Run("Meeus", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			for _, c := range cs {
+				astro.MeeusCalendarGregorianToJD(c.y, c.m, c.d)
+			}
+		}
+	})
+	b.Run("FF", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			for _, c := range cs {
+				astro.FFCalendarGregorianToJD(c.y, c.m, c.d)
+			}
+		}
+	})
+	b.Run("MJD", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			for _, c := range cs {
+				astro.CalendarGregorianToMJD(c.y, c.m, c.d)
+			}
+		}
+	})
+}
